838f20aa037707fc1983ba7a87574c279ee0c6c0
aleenatariq <leenabatool17@gmail.com>
2024-12-18 10:38:15 +0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+       name="play",
+       version="1.0",
+       description="Python",
+       executables=[Executable("C:\\Users\\AleenaTariq\\OneDrive - AlphaBOLD\\Desktop\\play\\tests\\test_main.py")]
+   )
